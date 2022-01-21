a7c8eaf0cea7f8ad70946d1f6a97bf5a3995ee54
mazkar27 <mazkar27@gmail.com>
2022-01-21 19:41:00 +0700

    add footer to detail

--- src/component/dashboard/DetailComponent.js
+++ src/component/dashboard/DetailComponent.js
@@ -2,12 +2,13 @@ import React, { useEffect } from "react";
 import "./style.css";
 import Header from "../global/Header";
 import CardDetail from "./cardDetail/CardDetail";
-
+import Footer from "../global/Footer";
 const DetailComponent = (props) => {
   return (
     <div className="globalContainer">
       <Header />
       <CardDetail />
+      <Footer />
     </div>
   );
 };
--- src/component/login/LoginComponent.js
+++ src/component/login/LoginComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../global/Footer";
 import CardLogin from "./cardLogin/cardLogin";
 
 const LoginComp = (props) => {
   return (
     <div className="globalContainer">
       <CardLogin />
+      <Footer />
     </div>
   );
 };
